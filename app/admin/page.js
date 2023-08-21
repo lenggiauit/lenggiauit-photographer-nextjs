d3324e9775a66a3d36440f91a5cd53e915648ac6
Lenggiauit <lenggiauit@gmail.com>
2023-08-21 13:57:17 -0400

    update code

--- app/admin/page.js
+++ app/admin/page.js
@@ -8,25 +8,6 @@ import { signInWithPopup, signOut } from 'firebase/auth'
 let appSetting = require('/appSetting.json')
 let appData = require('/data.json')
 
-var homeData = appData.find((x) => x.pageUrl == '/')
-
-export async function generateMetadata({ params, searchParams }, parent) {
-  if (homeData)
-    return {
-      title: homeData.metaData.title,
-      description: homeData.metaData.description,
-      icon: '/favicon.ico',
-      openGraph: {
-        images: appSetting.baseUrl + homeData.sliderData[0].src,
-        title: homeData.metaData.title,
-        description: homeData.metaData.description,
-        url: appSetting.baseUrl,
-      },
-    }
-  else {
-    return null
-  }
-}
 export default function Admin() {
   const signInWithGoogle = async () => {
     try {
