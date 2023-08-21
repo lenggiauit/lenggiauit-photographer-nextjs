65915998d5248b6714fae5193c98899621f1e7c2
Lenggiauit <lenggiauit@gmail.com>
2023-08-21 16:59:09 -0400

    update code

--- app/admin/page.js
+++ app/admin/page.js
@@ -4,11 +4,13 @@ import AdminBookingList from '@/components/adminBookingList'
 import { auth, googleProvider } from '../firebase'
 import SignIn from '@/components/auth'
 import { signInWithPopup, signInWithRedirect, signOut } from 'firebase/auth'
+import { useEffect, useState } from 'react'
 
 let appSetting = require('/appSetting.json')
 let appData = require('/data.json')
 
 export default function Admin() {
+  const [user, setUser] = useState(null)
   const signInWithGoogle = async () => {
     try {
       await signInWithRedirect(auth, googleProvider)
@@ -23,13 +25,29 @@ export default function Admin() {
       console.error(err)
     }
   }
+
+  useEffect(() => {
+    auth.onAuthStateChanged(
+      (user) => {
+        if (user) {
+          // User is signed in.
+          setUser(user)
+        } else {
+          // No user is signed in.
+          reject('no user logged in')
+        }
+      },
+      // Prevent console error
+      (error) => reject(error)
+    )
+  }, [])
+
   return (
     <>
       <div id='bt-wrapper' className='bt-wrapper bt-haslayout'>
         <Navigation black />
         <main id='bt-main' className='bt-main bt-sectionspace bt-haslayout'>
-          {auth.currentUser == null ||
-          auth.currentUser.email != 'lenggiauit@gmail.com' ? (
+          {user == null || user?.email != 'lenggiauit@gmail.com' ? (
             <SignIn signIn={signInWithGoogle} />
           ) : (
             <>
