33a4ddcaef0a32253e4e2c827194b548bbda3d28
Lenggiauit <lenggiauit@gmail.com>
2024-01-12 22:46:34 -0500

    update code-- update about page

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,6 @@
 import Navigation from '@/components/navigation'
-import HomeAlbums from '@/components/homeAlbums'
 import Chatbot from '@/components/chatbot'
+import BtnBooking from '@/components/btnBooking'
 let appSetting = require('/appSetting.json')
 let appData = require('/data/pages.json')
 const pageData = appData.find((x) => x.pageUrl == '/about')
@@ -26,136 +26,144 @@ export default function About() {
   return (
     <>
       <div id='bt-wrapper' className='bt-wrapper bt-haslayout'>
-        <Navigation />
-        <div className='bt-innerpagebanner bt-innerpagebannerv3'>
-          <div className='container-fluid'>
-            <div className='row'>
-              <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
-                <div className='bt-innerbannercontent'>
-                  <h1>about</h1>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
+        <Navigation black />
+        <BtnBooking />
         <main id='bt-main' className='bt-main bt-haslayout pt-4'>
           <div className='container-fluid'>
             <div className='row'>
-              <div className='col-xs-12 col-sm-12 col-md-12 col-lg-12'>
-                <div id='bt-content' className='bt-content'>
-                  <div className='bt-member bt-memberdetail'>
-                    <div className='bt-membercontent'>
-                      <div className='bt-membername'>
-                        <h3>Giau Le</h3>
-                      </div>
-                      <span className='bt-memberdesignation'>
-                        Full-Stack Developer &amp; Photographer
-                      </span>
-                      <h4>I’m a professional photographer</h4>
-                      <div className='bt-description'>
-                        <p>
-                          With a love for travel and exploration, I connect with
-                          people and share stories. My passion for discovery,
-                          both in new places and cuisines, brings me endless
-                          joy. Through my lens, I create captivating images that
-                          transport viewers to ethereal realms, capturing
-                          cherished moments with a touch of enchantment.
-                        </p>
-                      </div>
-                      <ul className='bt-socialicons'>
-                        <li>
-                          <a
-                            target='_blank'
-                            href='https://www.facebook.com/lenggiauit'
-                          >
-                            <i className='fa fa-facebook'></i>
-                          </a>
-                        </li>
-                        <li>
-                          <a
-                            target='_blank'
-                            href='https://www.instagram.com/lenggiauit/'
-                          >
-                            <i className='fa fa-instagram'></i>
-                          </a>
-                        </li>
-                      </ul>
-                    </div>
-                    <div className='bt-skillsandclients'>
-                      <div className='bt-skills'>
-                        <h3>My Skills</h3>
-                        <div id='bt-memberskills' className='bt-memberskills'>
-                          <div className='bt-memberskill'>
-                            <h4>Digital Photography</h4>
-                            <div className='progress'>
-                              <div
-                                className='progress-bar bg-dark'
-                                role='progressbar'
-                                style={{ width: '100%' }}
-                                aria-valuenow='25'
-                                aria-valuemin='0'
-                                aria-valuemax='100'
-                              ></div>
-                            </div>
-                          </div>
-                          <div className='bt-memberskill'>
-                            <h4>Wedding Photography</h4>
-                            <div className='progress'>
-                              <div
-                                className='progress-bar bg-dark'
-                                role='progressbar'
-                                style={{ width: '70%' }}
-                                aria-valuenow='25'
-                                aria-valuemin='0'
-                                aria-valuemax='100'
-                              ></div>
-                            </div>
-                          </div>
-                          <div className='bt-memberskill'>
-                            <h4>Portrait</h4>
-                            <div className='progress'>
-                              <div
-                                className='progress-bar bg-dark'
-                                role='progressbar'
-                                style={{ width: '90%' }}
-                                aria-valuenow='25'
-                                aria-valuemin='0'
-                                aria-valuemax='100'
-                              ></div>
-                            </div>
-                          </div>
-                          <div className='bt-memberskill'>
-                            <h4>Commercial</h4>
-                            <div className='progress'>
-                              <div
-                                className='progress-bar bg-dark'
-                                role='progressbar'
-                                style={{ width: '80%' }}
-                                aria-valuenow='25'
-                                aria-valuemin='0'
-                                aria-valuemax='100'
-                              ></div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div className='bt-clients mt-5'>
-                        <h3>Contact</h3>
-                        <ul className='w-100'>
-                          <li>
-                            <span>Email </span>
-                            <a href='lenggiauit@gmail.com'>
-                              lenggiauit@gmail.com
-                            </a>
-                          </li>
-                          <li>
-                            <span>Phone +1 437 410 3085</span>
-                          </li>
-                          <li>Toronto, Canada</li>
-                        </ul>
+              <div className='col-md-4'>
+                <div className='bt-innerbannercontent about '>
+                  <div className='avatar-frame-container'>
+                    <img
+                      className='avatar-image'
+                      src='/images/bg/giaule.jpg'
+                      alt='lenggiauit photographer'
+                    />
+                    <img
+                      src='/img/avatar-frame.png'
+                      alt='avatar'
+                      class='avatar-frame anim-spin'
+                    ></img>
+                  </div>
+                </div>
+              </div>
+              <div className='col-md-8 about-info'>
+                <div className='row'>
+                  <div className='col-md-12 '>
+                    <h3 className='about-name'>Giau Le</h3>
+                    <h4>
+                      I’m a Full-Stack Developer &amp; professional photographer
+                    </h4>
+                    <p>
+                      With a love for travel and exploration, I connect with
+                      people and share stories. My passion for discovery, both
+                      in new places and cuisines, brings me endless joy. Through
+                      my lens, I create captivating images that transport
+                      viewers to ethereal realms, capturing cherished moments
+                      with a touch of enchantment.
+                    </p>
+                  </div>
+                </div>
+                <div className='row'>
+                  <div className='col-md-12'>
+                    <ul className='bt-socialicons-about'>
+                      <li>
+                        <a
+                          target='_blank'
+                          href='https://www.facebook.com/lenggiauit'
+                        >
+                          <i
+                            className='fa fa-facebook'
+                            style={{ fontSize: 32 }}
+                          ></i>
+                        </a>
+                      </li>
+                      <li>
+                        <a
+                          target='_blank'
+                          href='https://www.instagram.com/lenggiauit/'
+                        >
+                          <i
+                            className='fa fa-instagram'
+                            style={{ fontSize: 32 }}
+                          ></i>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div className='row about-skills'>
+                  <div className='col-md-12'>
+                    <h3>My Skills</h3>
+
+                    <div className='bt-memberskill'>
+                      <h5>Digital Photography</h5>
+                      <div className='progress'>
+                        <div
+                          className='progress-bar bg-dark'
+                          role='progressbar'
+                          style={{ width: '90%' }}
+                          aria-valuenow='25'
+                          aria-valuemin='0'
+                          aria-valuemax='100'
+                        ></div>
                       </div>
                     </div>
+                    <div className='bt-memberskill'>
+                      <h5>Wedding Photography</h5>
+                      <div className='progress'>
+                        <div
+                          className='progress-bar bg-dark'
+                          role='progressbar'
+                          style={{ width: '70%' }}
+                          aria-valuenow='25'
+                          aria-valuemin='0'
+                          aria-valuemax='100'
+                        ></div>
+                      </div>
+                    </div>
+                    <div className='bt-memberskill'>
+                      <h5>Portrait</h5>
+                      <div className='progress'>
+                        <div
+                          className='progress-bar bg-dark'
+                          role='progressbar'
+                          style={{ width: '100%' }}
+                          aria-valuenow='25'
+                          aria-valuemin='0'
+                          aria-valuemax='100'
+                        ></div>
+                      </div>
+                    </div>
+                    <div className='bt-memberskill'>
+                      <h5>Commercial</h5>
+                      <div className='progress'>
+                        <div
+                          className='progress-bar bg-dark'
+                          role='progressbar'
+                          style={{ width: '80%' }}
+                          aria-valuenow='25'
+                          aria-valuemin='0'
+                          aria-valuemax='100'
+                        ></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+                <div className='row about-contact'>
+                  <div className='col-md-12'>
+                    <h3>Contact</h3>
+                    <ul className='w-100'>
+                      <li>
+                        <span>Email </span>
+                        <a href='lenggiauit@gmail.com'>lenggiauit@gmail.com</a>
+                      </li>
+                      <li>
+                        <span>Phone +1 437 410 3085</span>
+                      </li>
+                      <li>Toronto, Canada</li>
+                    </ul>
                   </div>
                 </div>
               </div>
